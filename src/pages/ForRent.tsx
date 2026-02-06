57ae3471fd98c2c2745a634d405a78cbd15634a5
Tarek Samoud <mr.tareksamoud@gmail.com>
2026-02-06 16:27:09 +0100

    implement the home page with demo data

--- src/pages/ForRent.tsx
+++ src/pages/ForRent.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useI18n } from '../i18n/I18nProvider';
 
 const ForRent: React.FC = () => {
+  const { t } = useI18n()
   return (
     <div>
-      <h1>ForRent</h1>
-      <p>This is the ForRent page.</p>
+      <h1>{t('pages.forRent')}</h1>
+      <p>{t('pages.placeholder')}</p>
     </div>
   );
 };
